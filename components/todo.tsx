2609d962e0ece6960c8ebc2a1fe6c5e13ed0eb35
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-05-16 00:54:52 +0900

    error update

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,10 +1,15 @@
 import { Checkbox, IconButton } from "@material-tailwind/react";
+import { useState } from "react"
 
-export default function Todo() {
+export default function Todo({ id, value }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [completed, setCompleted] = useState(false);
+    const [title, setTitle] = useState("");
+
     return (
-        <div>
-            <Checkbox />
-            <p>New Todo</p>
+        <div className="w-full flex items-center gap-1">
+            <Checkbox checked={completed} />
+            <p className={`flex-1 ${completed && "line-through"}`}>{value}</p>
             <IconButton><i className="fas fa-pen" /></IconButton>
             <IconButton><i className="fas fa-trash" /></IconButton>
         </div>
